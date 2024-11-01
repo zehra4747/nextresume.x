dd0d995f98b5d0e3f808c2081b378ff7046781f1
Zehra <163136295+zehra4747@users.noreply.github.com>
2024-11-01 19:35:42 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+
+export default function Resume(){
+  return(
+      <div className="font-serif">
+          
+
+<div className="space-y-4  items-center  bg-[rgb(46,11,43)] text-white p-[40px] text-center mb-4">
+          <h1 className="text-3xl font-bold flex justify-center">MAIMOONA ZEHRA</h1>
+
+  <p className="text-1xl font-semibold flex justify-center items-center ">An Aspiring Web-Developer</p>     
+  </div>
+  <div className="mb-4 "> <p className="font-serif text-base text-lg leading-relaxed text-center px-40">A driven student pursuing Bachelor's in Computational Finance at NED University, currently in 3rd Semester.As a dedicated member of university society's Event Management team,I balance academics wth organizing impactful events,teamwork and creativity.I'm also developing skills in Generative AI at GIAIC </p>
+</div>
+      <h2 className="mb-4 text-white text-bold bg-[rgb(117,92,110)] p-2">
+         CONTACT
+      
+      </h2>
+      <p> Phone NO: 03182554441</p>
+      <p>Email: maimoonazehra47@gmail.com</p>
+      <p>LinkedIn: Maimoona Zehra</p>
+      <p className="mb-4">Github: Zehra4747</p>
+
+      <h2 className="text-white bg-[rgb(117,92,110)] p-2 mb-4">
+          SKILLS
+      </h2>
+      <ul>
+      <p> HTML</p>
+      <p> CSS</p>
+      
+      <p>NEXTJS</p>
+      <p className="mb-4">TYPESCRIPT</p>
+      </ul>
+
+
+      <h2 className="mb-4 text-white bg-[rgb(117,92,110)] p-2">
+       EDUCATION   
+      </h2>
+      <p>
+         <b>BS-UNDERGRADUATE</b>:NED UNIVERSITY (Computational Finance)</p>
+        <p> <b>FCS</b>:SIR SYED COLLEGE FOR WOMEN(Pre-Engineering)</p>
+        <p className="mb-8"> <b>MATRIC</b>:SIR SYED CHILDRENS ACADEMY(Science)
+      </p>
+
+       <h2 className="mb-4 text-white bg-[rgb(117,92,110)] p-2">
+       EXPERIENCE
+      </h2>
+      <p className="mb-6">
+          No Experience
+        </p>
+      </div>
+  )
+}
